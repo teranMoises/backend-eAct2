88e441193fef11cffec2763bf5eef5e72db56be9
Moisés Alfonso Terán <teranrivasma@uvm.edu.ve>
2023-11-20 13:59:37 -0400

    Mostrar errores en views

--- routes/patrocinadores.js
+++ routes/patrocinadores.js
@@ -73,51 +73,58 @@ router.put('/:index', function (req, res, next) {
 
 /* VIEWS */
 
-router.get('/nuevoPatrocinador',function(req, res, next){
-   Patrocinador_Controller.ver_patrocinios().then((resultados)=>{
-      Equipos_Controller.ver_equipos_sin_padrino().then((equipos)=>{
-        let patrocinios = resultados;
-        let id_equipos = equipos
-        res.render('nuevoPatrocinador',{title: 'Crear un Patrocinador', patrocinios: patrocinios, equipos: id_equipos});
-     }).catch((error)=>{
-         res.status(500).send(error)
-     })
+router.get('/nuevoPatrocinador', function (req, res, next) {
+   Patrocinador_Controller.ver_patrocinios().then((resultados) => {
+      Equipos_Controller.ver_equipos_sin_padrino().then((equipos) => {
+         let patrocinios = resultados;
+         let id_equipos = equipos
+         res.render('nuevoPatrocinador', { title: 'Crear un Patrocinador', patrocinios: patrocinios, equipos: id_equipos });
+      }).catch((error) => {
+         if (error.codigo && error.mensaje) { res.status(error.codigo).send(error.mensaje) }
+         else { res.status(500).send(error) }
+      })
       //console.log(resultados)
-   }).catch((error)=>{
-      res.status(500).send(error)
+   }).catch((error) => {
+      if (error.codigo && error.mensaje) { res.status(error.codigo).send(error.mensaje) }
+      else { res.status(500).send(error) }
    })
 })   
 
 router.post('/nuevoPatrocinador', function (req, res, next) {
-   if(req.body.idPatrocinio == 5){  
-      if(req.body.idEquipo != ""){
-         Patrocinador_Controller.ingresar_patrocinador(req.body).then((resultado)=>{
-            Patrocinador_Controller.ingresar_padrino(resultado).then(()=>{
-               Patrocinador_Controller.ver_patrocinador().then((resultados)=>{
+   if (req.body.idPatrocinio == 5) {
+      if (req.body.idEquipo != "") {
+         Patrocinador_Controller.ingresar_patrocinador(req.body).then((resultado) => {
+            Patrocinador_Controller.ingresar_padrino(resultado).then(() => {
+               Patrocinador_Controller.ver_patrocinador().then((resultados) => {
                   res.json(resultados);
-               }).catch((error)=>{
-                  res.status(500).send(error)
+               }).catch((error) => {
+                  if (error.codigo && error.mensaje) { res.status(error.codigo).send(error.mensaje) }
+                  else { res.status(500).send(error) }
                })
-            }).catch((error)=>{
-               res.status(500).send(error)
-            }) 
-         }).catch((error)=>{
-            res.status(500).send(error)
+            }).catch((error) => {
+               if (error.codigo && error.mensaje) { res.status(error.codigo).send(error.mensaje) }
+               else { res.status(500).send(error) }
+            })
+         }).catch((error) => {
+            if (error.codigo && error.mensaje) { res.status(error.codigo).send(error.mensaje) }
+            else { res.status(500).send(error) }
          })
-      }else{
-         res.status(500).send("Para ser un padrino debe de patrocinar algún equipo")
+      } else {
+         res.status(404).send("Para ser un padrino debe de patrocinar algún equipo")
       }
-  }else{
-      Patrocinador_Controller.ingresar_patrocinador(req.body).then(()=>{
-         Patrocinador_Controller.ver_patrocinador().then((resultados)=>{
+   } else {
+      Patrocinador_Controller.ingresar_patrocinador(req.body).then(() => {
+         Patrocinador_Controller.ver_patrocinador().then((resultados) => {
             res.json(resultados);
-         }).catch((error)=>{
-            res.status(500).send(error)
+         }).catch((error) => {
+            if (error.codigo && error.mensaje) { res.status(error.codigo).send(error.mensaje) }
+            else { res.status(500).send(error) }
          })
-      }).catch((error)=>{
-         res.status(500).send(error)
+      }).catch((error) => {
+         if (error.codigo && error.mensaje) { res.status(error.codigo).send(error.mensaje) }
+         else { res.status(500).send(error) }
       })
-  }
+   }
 }); 
 
 module.exports = router; 
